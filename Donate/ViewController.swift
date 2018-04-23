2cbe5f6ecd5af0238ee152f469073720a2417489
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-04-23 13:25:09 -0600

    Initial Commit

---
+++ Donate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Donate
+//
+//  Created by Shaun Seidman on 4/23/18.
+//  Copyright © 2018 Shaun Seidman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
