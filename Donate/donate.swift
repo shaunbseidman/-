9359ba1a4ada7bb18aced4ce9c03d55de5064913
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-04-23 15:34:54 -0600

    Can add items

--- Donate/donate.swift
+++ Donate/donate.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class donateViewController: UITableViewController {
     
-    let itemArray = ["Blankets", "Cans", "Pens"]
+    var itemArray = ["Blankets", "Cans", "Pens"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +26,6 @@ class donateViewController: UITableViewController {
         return cell
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
-        
-        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else{
@@ -36,5 +33,25 @@ class donateViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Enter New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
 
