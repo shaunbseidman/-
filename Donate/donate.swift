73426d3210c67f5b1102d5a8abf0468980e9883b
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-04-23 14:42:22 -0600

    created first list

---
+++ Donate/donate.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Donate
+//
+//  Created by Shaun Seidman on 4/23/18.
+//  Copyright © 2018 Shaun Seidman. All rights reserved.
+//
+
+import UIKit
+
+class donateViewController: UITableViewController {
+    
+    let itemArray = ["Blankets", "Cans", "Pens"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "donateCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
