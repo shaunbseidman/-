97e92e1020d27e32d627bfc9e62df0734ad55c7e
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-04-23 19:32:36 -0600

    creating mvc

--- Donate/Controllers/donate.swift
+++ Donate/Controllers/donate.swift
@@ -10,12 +10,26 @@ import UIKit
 
 class donateViewController: UITableViewController {
     
-    var itemArray = ["Blankets", "Cans", "Pens"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "donateArray") as? [String]{
+        
+        let newItem = Item()
+        newItem.title = "Blankets"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Pens"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Food"
+        itemArray.append(newItem3)
+        
+        
+        if let items = defaults.array(forKey: "donateArray") as? [Item]{
             itemArray = items
         }
         
@@ -24,17 +38,22 @@ class donateViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
          return itemArray.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "donateCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
         return cell
     }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -43,7 +62,9 @@ class donateViewController: UITableViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "donateArray")
             self.tableView.reloadData()
             
---
+++ Donate/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Donate
+//
+//  Created by Shaun Seidman on 4/23/18.
+//  Copyright © 2018 Shaun Seidman. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
