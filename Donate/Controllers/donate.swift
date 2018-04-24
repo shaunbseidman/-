a7ee0140a2418d737a49f00e859938fa1bbdcc0c
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-04-23 20:33:52 -0600

    local storage

--- Donate/Controllers/donate.swift
+++ Donate/Controllers/donate.swift
@@ -11,27 +11,17 @@ import UIKit
 class donateViewController: UITableViewController {
     
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(dataFilePath)
+        loadItems()
         
-        let newItem = Item()
-        newItem.title = "Blankets"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Pens"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Food"
-        itemArray.append(newItem3)
-        
-        
-        if let items = defaults.array(forKey: "donateArray") as? [Item]{
-            itemArray = items
-        }
+//
+//        if let items = defaults.array(forKey: "donateArray") as? [Item]{
+//            itemArray = items
+//        }
         
     }
 
@@ -41,19 +31,15 @@ class donateViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "donateCell", for: indexPath)
-        
         let item = itemArray[indexPath.row]
-
         cell.textLabel?.text = item.title
-        
         cell.accessoryType = item.done == true ? .checkmark : .none
-        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-        tableView.reloadData()
+        self.saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -65,19 +51,36 @@ class donateViewController: UITableViewController {
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "donateArray")
-            self.tableView.reloadData()
-            
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Enter New Item"
             textField = alertTextField
+            self.saveItems()
         }
-        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
     
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        self.tableView.reloadData()
+    }
     
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("error decoding item array, \(error)")
+            }
+        }
+    }
 }
 
