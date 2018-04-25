b1029f228e240f349e56f045e85f40dda55560bc
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-04-24 20:57:52 -0600

    add first menu layer

--- Donate/Controllers/CategoryViewController.swift
+++ Donate/Controllers/CategoryViewController.swift
@@ -10,51 +10,35 @@ import UIKit
 import CoreData
 
 class CategoryViewController: UITableViewController {
-    
     var categories = [Category]()
-    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadCategories()
-
     }
 
-//    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
-//    }
     //MARK: - Tableview Datasource  Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        
         cell.textLabel?.text = categories[indexPath.row].name
-        
         return cell
     }
 
     
     //MARK: - Tableview Delegate Methods
-
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: " goToItems", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! donateViewController
-        
         if let indexPath = tableView.indexPathForSelectedRow {
             destinationVC.selectedCategory = categories[indexPath.row]
         }
     }
-    
-    
-     
     
     //MARK: - Data Manipulation Methods
     func saveCategories(){
@@ -68,7 +52,6 @@ class CategoryViewController: UITableViewController {
     
     func loadCategories(){
         let request :NSFetchRequest<Category> = Category.fetchRequest()
-        
         do{
             categories =  try context.fetch(request)
         } catch{
@@ -83,12 +66,9 @@ class CategoryViewController: UITableViewController {
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            
             let newCategory = Category(context: self.context)
             newCategory.name = textField.text!
-            
             self.categories.append(newCategory)
-            
             self.saveCategories()
 
         }
--- Donate/Controllers/donate.swift
+++ Donate/Controllers/donate.swift
@@ -50,9 +50,7 @@ class donateViewController: UITableViewController  {
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
 //            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -82,7 +80,15 @@ class donateViewController: UITableViewController  {
         self.tableView.reloadData()
     }
     
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil){
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let additionalPredicate = predicate{
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else{
+            request.predicate = categoryPredicate
+        }
+//        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, predciate])
+//        request.predicate = compoundPredicate
         do{
             itemArray = try context.fetch(request)
         } catch{
@@ -96,9 +102,9 @@ class donateViewController: UITableViewController  {
 extension donateViewController: UISearchBarDelegate{
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         let request : NSFetchRequest<Item> = Item.fetchRequest()
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
